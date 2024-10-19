e3d7728b9791b82ded3edf365ba8d687f035a11f
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-10-19 18:29:46 -0300

    add all the proyect

---
+++ myApp.js
@@ -0,0 +1,52 @@
+let express = require('express');
+let app = express();
+require('dotenv').config();
+
+console.log("Hello World")
+
+app.use((req, res, next)=> {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
+})
+
+app.get('/', (req, res)=> {
+    res.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/json', (req, res)=> {
+    process.env.MESSAGE_STYLE === 'uppercase'
+        ? res.json({"message": "HELLO JSON"})
+        : res.json({"message": "Hello json"})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
